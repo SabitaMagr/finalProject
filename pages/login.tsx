a30b43f197285024b180d85d6ba4440f4b4509d4
SabitaMagr <sabitathapa9864@gmail.com>
2023-03-03 08:00:56 +0545

    --

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,17 +6,10 @@ import React, { useState } from 'react'
 const login = () => {
   const [state, setState] = useState<boolean>(false);
   return (
-<<<<<<< HEAD
      <div className="flex mx-auto p-16  justify-start pl-[10rem] min-h-screen  w-[100%] h-[100%] !bg-no-repeat !bg-cover !bg-center " style={{background:'url(/images/Login.avif)'}}> 
-       {
-!state?
-      <LoginForm setState={setState} state={state}/>
-=======
-    <div className="flex mx-auto p-16 justify-start pl-[10rem] min-h-screen  w-[100%] h-[100%] !bg-no-repeat !bg-cover !bg-center " style={{ background: 'url(/images/Login.avif)' }}>
       {
         state ?
           <LoginForm setState={setState} state={state} />
->>>>>>> 46b6e935e2e0daf5fbb95721b9c7082c85a1e575
 
           :
           <RegitserForm />
